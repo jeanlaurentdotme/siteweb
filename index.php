031dd05af76faec07bbee00b3a3c1a6e7d289545
LAURENT Jean <laurentjean678@gmail.com>
2021-12-08 09:58:14 +0100

    ajout index

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC> <!--définir le type de document-->
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html" />
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Page WEB SNIR AP 2021</title>
+</head>
+
+<body>
+	<div class="page">
+		<div class="zone1">
+			<div class="photo"><img src="https://media-exp1.licdn.com/dms/image/C4D03AQFawl4FGBGKug/profile-displayphoto-shrink_200_200/0/1633528724259?e=1644451200&v=beta&t=wJGlLHI5fRvjpkm53dp6xRa5nCS88yQhGnvEgzSW7yY" alt="ma photo" width="180" height="180" /></div>
+			<div class="texte">
+				<p>Page WEB SNIR AP</p>
+				<p>LAURENT Jean</p>
+			</div>
+		</div>
+
+		<div class="zone2">
+			<h1> exemple 1 : les sections</h1>
+			<ul>
+				<li><a target="">Exemple de texte</a></li>
+			</ul>
+		</div>
+
+		<div class="zone3">
+			<p>exemple 1 : les liens </p>
+			<p> </p>
+			<ul>
+				<a href="fichiers/exemple.dat">Cliquer ici pour &ecirctre redirig&eacute</a>
+			</ul>
+		</div> 
+
+
+		<div class="zone4">
+			<p>Date de cr&eacuteation : 17/11/2021</p>
+			<p>Derni&egravere mise &agrave jour le : 08/12/2021</p>
+		</div>
+	</div>
+</body>
+</html>
+
